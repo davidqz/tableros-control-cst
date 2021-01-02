cea14520e623f5e5917e58b386af90eb87cf1b35
David Quiroz Marin <42786987+davidqz@users.noreply.github.com>
2021-01-01 22:51:29 -0600

    Lectura de tablas de 2019
    Agregar las tablas de 2019 al concentrado

--- scripts/accumulate_and_filter_data.py
+++ scripts/accumulate_and_filter_data.py
@@ -14,6 +14,14 @@ IMPORTANT_FIELDS = {
     'Proyectos': ['Adscripción', 'Área responsable', 'Sector'],
     'OtrasActividades': ['Adscripción', ],
 }
+EXTRA_FILES = {
+    'Articulos': ['2019_S1/1. Publicaciones.xlsx', '2019_S2/1. Publicaciones.xlsx', '2020_S1/01. Publicaciones realizadas.xls'],
+    'Conferencias Tecnicas': ['2019_S1/7. Conferencias.xlsx', '2019_S2/7. Conferencias.xlsx', ],
+    'Consultoria': ['2019_S1/4. Asesorías y consultorías.xlsx', '2019_S2/4. Asesorias y consultorias.xlsx', ],
+    'Cursos': ['2019_S1/5. Cursos.xlsx', '2019_S2/5. Cursos.xlsx', ],
+    'Proyectos': ['2019_S1/3. Proyectos de desarrollo tecnológico.xlsx', '2019_S2/3. Proyectos de desarrollo tecnologico.xlsx', ],
+    'OtrasActividades': ['2019_S1/18. Otras actividades relevantes.xlsx', '2019_S2/18. Otras actividades relevantes.xlsx', ],
+}
 AREA_MAPPING = {
     'MY': 'Monterrey',
     'AG': 'Aguascalientes',
@@ -46,24 +54,53 @@ file_titles = [title.split('/')[3].split('.')[0].strip() for title in file_paths
 # Add a DataFrame (table) for each file into a dictionary with file_titles as keys
 data_frames = {file_titles[i]: pandas.read_excel(path) for i, path in enumerate(file_paths)}
 
-# Read data from 2019
-extra_data_frame = pandas.read_excel('../data/2019_S2/1. Publicaciones.xlsx')
+# Read extra data from 2019_S1
+# extra_data_frames = {name: pandas.read_excel(f'data/{path[0]}') for name, path in EXTRA_FILES.items()}
+extra_data_frames = {}
+for name, path in EXTRA_FILES.items():
+    s1 = pandas.read_excel(f'../data/{path[0]}')
+    s2 = pandas.read_excel(f'../data/{path[1]}')
+    extra_data_frames[name] = pandas.concat([s1, s2])
 
 # Data Cleaning reformatting
-for file_title, df in data_frames.items():
+for file, df in data_frames.items():
     # Remove semester suffix ('1_', '2_') from all YEAR_LABEL columns (leaving just the year)
     df[YEAR_LABEL] = df[YEAR_LABEL].str.slice_replace(stop=2)
     # Map 'Adscripción' codes to areas from AREA_MAPPING
-    df[IMPORTANT_FIELDS[file_title][0]] = df[IMPORTANT_FIELDS[file_title][0]].apply(
+    df[IMPORTANT_FIELDS[file][0]] = df[IMPORTANT_FIELDS[file][0]].apply(
         lambda s: AREA_MAPPING[s.split(' ')[1]]
     )
     # print(df[IMPORTANT_FIELDS[file_title][0]])
 
+# Remove U or Unidad and correct Monterrrey
+def format_adscripcion(name):
+    split_name = name.split(' ')
+    if len(split_name) > 1:
+        if split_name[1] == 'Monterrrey':
+            return 'Monterrey'
+        else:
+            return split_name[1]
+    else:
+        if name in AREA_MAPPING.keys():
+            return AREA_MAPPING[name]
+        else:
+            return name
 
-def add_count_columns(field_label, table, output_dic, output_column_labels):
+for file, df in extra_data_frames.items():
+    df[IMPORTANT_FIELDS[file][0]] = df[IMPORTANT_FIELDS[file][0]].apply(format_adscripcion)
+
+
+def add_count_columns_multiyear(field_label, table, extra_table, output_dic, output_column_labels):
     # Get all different values for field_label and their counts
     field_values_count = table[field_label].value_counts()
     field_values = list(field_values_count.index)
+    # Add the extra columns from 2019-2020
+    extra_field_values_count = extra_table[field_label].value_counts()
+    extra_field_values = list(extra_field_values_count.index)
+    for f in extra_field_values:
+        if f not in field_values:
+            field_values.append(f)
+
     field_values.sort()
 
     # Separate rows from table by year in a dictionary with years as keys
@@ -72,7 +109,7 @@ def add_count_columns(field_label, table, output_dic, output_column_labels):
     # Add to output_column_labels concatenating the field with each unique value
     output_column_labels.extend(f'{field_label}.{value}' for value in field_values)
 
-    if field_label == 'Adscripción' or field_label == 'Depa218':
+    if field_label == 'Adscripción':
         for f in field_values:
             adscripciones[f] = 0
 
@@ -88,6 +125,7 @@ def add_count_columns(field_label, table, output_dic, output_column_labels):
 
 def create_count_table(file_title):
     table = data_frames[file_title]
+    extra_table = extra_data_frames[file_title]
     fields = IMPORTANT_FIELDS[file_title]
 
     # Get total entries per year
@@ -104,8 +142,20 @@ def create_count_table(file_title):
     output_column_labels = ['Total']
 
     for field in fields:
-        add_count_columns(field_label=field, table=table, output_dic=output_dic,
-                          output_column_labels=output_column_labels)
+        add_count_columns_multiyear(table=table, extra_table=extra_table, field_label=field, output_dic=output_dic,
+                                    output_column_labels=output_column_labels)
+
+    # Add 2019_S1 data
+    for label in output_column_labels:
+        if label == 'Total':
+            output_dic['2019'] = [len(extra_table.index)]
+        else:
+            if label.startswith('Adscripción'):
+                area = label.split('.')[1]
+                counts_by_area = len(extra_table[extra_table['Adscripción'] == area].index)
+                output_dic['2019'].append(counts_by_area)
+            else:
+                output_dic['2019'].append(0)
 
     output_table = pandas.DataFrame.from_dict(output_dic, orient='index', columns=output_column_labels)
     output_table.to_csv(f'../data/output/{file_title}.csv', index_label=YEAR_LABEL)
