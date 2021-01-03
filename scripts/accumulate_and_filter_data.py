f3500b06aa212d3e612b116c26d555167763e805
David Quiroz Marin <42786987+davidqz@users.noreply.github.com>
2021-01-02 18:00:42 -0600

    Lectura y proceso de datos 2020
    Agregar los datos faltantes de 2020

--- scripts/accumulate_and_filter_data.py
+++ scripts/accumulate_and_filter_data.py
@@ -5,22 +5,30 @@ import matplotlib.pyplot as plt
 
 # Global constants
 YEAR_LABEL = 'Periodo'
-YEARS = ['2016', '2017', '2018']
+YEARS = ['2016', '2017', '2018', '2019', '2020']
 IMPORTANT_FIELDS = {
     'Articulos': ['Adscripción', 'Categoría', 'Circulación de la revista'],
     'Conferencias Tecnicas': ['Adscripción', 'Alcance', 'Naturaleza de la conferencia'],
-    'Consultoria': ['Adscripción', 'Área responsable', 'Sector'],
-    'Cursos': ['Adscripción', 'Área responsable', 'Sector'],
-    'Proyectos': ['Adscripción', 'Área responsable', 'Sector'],
+    'Consultoria': ['Adscripción', 'Sector'],
+    'Cursos': ['Adscripción', 'Sector'],
+    'Proyectos': ['Adscripción', 'Sector'],
     'OtrasActividades': ['Adscripción', ],
 }
 EXTRA_FILES = {
-    'Articulos': ['2019_S1/1. Publicaciones.xlsx', '2019_S2/1. Publicaciones.xlsx', '2020_S1/01. Publicaciones realizadas.xls'],
-    'Conferencias Tecnicas': ['2019_S1/7. Conferencias.xlsx', '2019_S2/7. Conferencias.xlsx', ],
-    'Consultoria': ['2019_S1/4. Asesorías y consultorías.xlsx', '2019_S2/4. Asesorias y consultorias.xlsx', ],
-    'Cursos': ['2019_S1/5. Cursos.xlsx', '2019_S2/5. Cursos.xlsx', ],
-    'Proyectos': ['2019_S1/3. Proyectos de desarrollo tecnológico.xlsx', '2019_S2/3. Proyectos de desarrollo tecnologico.xlsx', ],
-    'OtrasActividades': ['2019_S1/18. Otras actividades relevantes.xlsx', '2019_S2/18. Otras actividades relevantes.xlsx', ],
+    'Articulos': ['2019_S1/1. Publicaciones.xlsx', '2019_S2/1. Publicaciones.xlsx',
+                  '2020_S1/01. Publicaciones realizadas.xls'],
+    'Conferencias Tecnicas': ['2019_S1/7. Conferencias.xlsx', '2019_S2/7. Conferencias.xlsx',
+                              '2020_S1/18. Conferencias impartidas.xls'],
+    'Consultoria': ['2019_S1/4. Asesorías y consultorías.xlsx', '2019_S2/4. Asesorias y consultorias.xlsx',
+                    '2020_S1/04. Asesorías y consultorías realizadas.xls'],
+    'Cursos': ['2019_S1/5. Cursos.xlsx', '2019_S2/5. Cursos.xlsx',
+               '2020_S1/05. Cursos y capacitaciones impartidas.xls'],
+    'Proyectos': ['2019_S1/3. Proyectos de desarrollo tecnológico.xlsx',
+                  '2019_S2/3. Proyectos de desarrollo tecnologico.xlsx',
+                  '2020_S1/03. Proyectos de desarrollo tecnológico realizados.xls'],
+    'OtrasActividades': ['2019_S1/18. Otras actividades relevantes.xlsx',
+                         '2019_S2/18. Otras actividades relevantes.xlsx',
+                         '2020_S1/21. Otras actividades relevantes.xls'],
 }
 AREA_MAPPING = {
     'MY': 'Monterrey',
@@ -37,14 +45,14 @@ AREA_MAPPING = {
     'GCE': 'Consultoría Estadística',
     'GE': 'Consultoría Estadística',
 }
-TABLE_INDEX = 0  # Articulos
+# TABLE_INDEX = 0  # Articulos
 # TABLE_INDEX = 1  # Conferencias Tecnicas
 # TABLE_INDEX = 2  # Consultoria
 # TABLE_INDEX = 3  # Cursos
 # TABLE_INDEX = 4  # Otras Actividades
-# TABLE_INDEX = 5  # Proyectos
+TABLE_INDEX = 5  # Proyectos
 
-FIELD_INDEX = 0
+FIELD_INDEX = 1
 
 # Read data 2016-2018
 file_paths = glob.glob('../data/2016-2018/*.xls*')
@@ -54,13 +62,14 @@ file_titles = [title.split('/')[3].split('.')[0].strip() for title in file_paths
 # Add a DataFrame (table) for each file into a dictionary with file_titles as keys
 data_frames = {file_titles[i]: pandas.read_excel(path) for i, path in enumerate(file_paths)}
 
-# Read extra data from 2019_S1
-# extra_data_frames = {name: pandas.read_excel(f'data/{path[0]}') for name, path in EXTRA_FILES.items()}
-extra_data_frames = {}
+# Read extra data from 2019-2020
+data_frames_19 = {}
+data_frames_20 = {}
 for name, path in EXTRA_FILES.items():
-    s1 = pandas.read_excel(f'../data/{path[0]}')
-    s2 = pandas.read_excel(f'../data/{path[1]}')
-    extra_data_frames[name] = pandas.concat([s1, s2])
+    s1 = pandas.read_excel(f'../data/{path[0]}')  # 2019_S1
+    s2 = pandas.read_excel(f'../data/{path[1]}')  # 2019_S2
+    data_frames_19[name] = pandas.concat([s1, s2])
+    data_frames_20[name] = pandas.read_excel(f'../data/{path[2]}')  # 2020_S1
 
 # Data Cleaning reformatting
 for file, df in data_frames.items():
@@ -70,34 +79,48 @@ for file, df in data_frames.items():
     df[IMPORTANT_FIELDS[file][0]] = df[IMPORTANT_FIELDS[file][0]].apply(
         lambda s: AREA_MAPPING[s.split(' ')[1]]
     )
-    # print(df[IMPORTANT_FIELDS[file_title][0]])
+
 
 # Remove U or Unidad and correct Monterrrey
 def format_adscripcion(name):
     split_name = name.split(' ')
     if len(split_name) > 1:
-        if split_name[1] == 'Monterrrey':
-            return 'Monterrey'
-        else:
-            return split_name[1]
+        new_name = split_name[1]
     else:
-        if name in AREA_MAPPING.keys():
-            return AREA_MAPPING[name]
-        else:
-            return name
+        new_name = name
+    if new_name == 'Monterrrey':
+        return 'Monterrey'
+    if new_name in AREA_MAPPING.keys():
+        return AREA_MAPPING[new_name]
+    else:
+        return new_name
 
-for file, df in extra_data_frames.items():
+
+for file, df in data_frames_19.items():
+    df[IMPORTANT_FIELDS[file][0]] = df[IMPORTANT_FIELDS[file][0]].apply(format_adscripcion)
+
+for file, df in data_frames_20.items():
+    # Rename columns 'Área de adscripción' and 'Area adscripción' to 'Adscripción'
+    df.rename(columns={'Área de adscripción': 'Adscripción', 'Area adscripción': 'Adscripción'}, inplace=True)
     df[IMPORTANT_FIELDS[file][0]] = df[IMPORTANT_FIELDS[file][0]].apply(format_adscripcion)
 
 
-def add_count_columns_multiyear(field_label, table, extra_table, output_dic, output_column_labels):
+def add_count_columns_multiyear(field_label, table, table_19, table_20, output_dic, output_column_labels):
     # Get all different values for field_label and their counts
     field_values_count = table[field_label].value_counts()
     field_values = list(field_values_count.index)
-    # Add the extra columns from 2019-2020
-    extra_field_values_count = extra_table[field_label].value_counts()
-    extra_field_values = list(extra_field_values_count.index)
-    for f in extra_field_values:
+
+    # Add the missing columns from 2019
+    table_19_field_values_count = table_19[field_label].value_counts()
+    table_19_field_values = list(table_19_field_values_count.index)
+    for f in table_19_field_values:
+        if f not in field_values:
+            field_values.append(f)
+
+    # Add the missing columns from 2020
+    table_20_field_values_count = table_20[field_label].value_counts()
+    table_20_field_values = list(table_20_field_values_count.index)
+    for f in table_20_field_values:
         if f not in field_values:
             field_values.append(f)
 
@@ -108,6 +131,7 @@ def add_count_columns_multiyear(field_label, table, extra_table, output_dic, out
 
     # Add to output_column_labels concatenating the field with each unique value
     output_column_labels.extend(f'{field_label}.{value}' for value in field_values)
+    # output_column_labels.extend(field_values)
 
     if field_label == 'Adscripción':
         for f in field_values:
@@ -125,7 +149,8 @@ def add_count_columns_multiyear(field_label, table, extra_table, output_dic, out
 
 def create_count_table(file_title):
     table = data_frames[file_title]
-    extra_table = extra_data_frames[file_title]
+    table_19 = data_frames_19[file_title]
+    table_20 = data_frames_20[file_title]
     fields = IMPORTANT_FIELDS[file_title]
 
     # Get total entries per year
@@ -142,20 +167,24 @@ def create_count_table(file_title):
     output_column_labels = ['Total']
 
     for field in fields:
-        add_count_columns_multiyear(table=table, extra_table=extra_table, field_label=field, output_dic=output_dic,
-                                    output_column_labels=output_column_labels)
+        add_count_columns_multiyear(table=table, table_19=table_19, table_20=table_20, field_label=field,
+                                    output_dic=output_dic, output_column_labels=output_column_labels)
 
-    # Add 2019_S1 data
+    # Add 2019-2020 data
     for label in output_column_labels:
         if label == 'Total':
-            output_dic['2019'] = [len(extra_table.index)]
+            output_dic['2019'] = [len(table_19.index)]
+            output_dic['2020'] = [len(table_20.index)]
         else:
             if label.startswith('Adscripción'):
                 area = label.split('.')[1]
-                counts_by_area = len(extra_table[extra_table['Adscripción'] == area].index)
-                output_dic['2019'].append(counts_by_area)
+                counts_by_area_19 = len(table_19[table_19['Adscripción'] == area].index)
+                counts_by_area_20 = len(table_20[table_20['Adscripción'] == area].index)
+                output_dic['2019'].append(counts_by_area_19)
+                output_dic['2020'].append(counts_by_area_20)
             else:
                 output_dic['2019'].append(0)
+                output_dic['2020'].append(0)
 
     output_table = pandas.DataFrame.from_dict(output_dic, orient='index', columns=output_column_labels)
     output_table.to_csv(f'../data/output/{file_title}.csv', index_label=YEAR_LABEL)
@@ -169,22 +198,22 @@ count_tables = [create_count_table(file) for file in file_titles]
 # print(len(adscripciones.keys()))
 # print(adscripciones.keys())
 
-# table_name = file_titles[TABLE_INDEX]
-# count_table = count_tables[TABLE_INDEX]
-#
-# field = IMPORTANT_FIELDS[table_name][FIELD_INDEX]
-# field_labels = count_table.columns[count_table.columns.str.startswith(field)]
-# for y in YEARS:
-#     plt.figure(f'{table_name} por {field} {y}', figsize=(8, 6))
-#     pie_values = count_table.loc[y, field_labels]
-#     pie_values = pie_values[pie_values > 0]
-#     total_values = sum(pie_values)
-#     # format_labels = pie_values.index.str.split('.')
-#     # print(format_labels)
-#     plt.pie(pie_values.values, labels=pie_values.index,
-#             autopct=lambda p: f'{int(p * total_values / 100)}')
-#
-# plt.figure(table_name, figsize=(8, 6))
-# plt.bar(count_table.index, count_table['Total'])
+table_name = file_titles[TABLE_INDEX]
+count_table = count_tables[TABLE_INDEX]
 
-# plt.show()
+field = IMPORTANT_FIELDS[table_name][FIELD_INDEX]
+field_labels = count_table.columns[count_table.columns.str.startswith(field)]
+for y in YEARS:
+    plt.figure(f'{table_name} por {field} {y}', figsize=(8, 6))
+    pie_values = count_table.loc[y, field_labels]
+    pie_values = pie_values[pie_values > 0]
+    total_values = sum(pie_values)
+    # format_labels = pie_values.index.str.split('.')
+    # print(format_labels)
+    plt.pie(pie_values.values, labels=pie_values.index,
+            autopct=lambda p: f'{int(p * total_values / 100)}')
+
+plt.figure(table_name, figsize=(8, 6))
+plt.bar(count_table.index, count_table['Total'])
+
+plt.show()
